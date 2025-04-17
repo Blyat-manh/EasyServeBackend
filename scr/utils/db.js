dac9261cce6d1fecf95eda780f6bfa749a30aca7
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2025-04-17 16:43:16 +0200

    Add files via upload

--- scr/utils/db.js
+++ scr/utils/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'nozomi.proxy.rlwy.net',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'QosWINxBAdmUnLLblfCubgMcQzCpUlWN',
-  database: process.env.DB_NAME || 'tpv',
-  port: process.env.DB_PORT || 3306
+  host: 'nozomi.proxy.rlwy.net',
+  user: 'root',
+  password: 'QosWINxBAdmUnLLblfCubgMcQzCpUlWN',
+  database: 'tpv',
+  port: 3306
 });
 
 module.exports = pool;

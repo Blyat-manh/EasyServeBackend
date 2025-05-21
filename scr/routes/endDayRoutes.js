46053aef9b3dbd47b29f0c0c9b477a3e2f470601
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2025-05-21 19:03:49 +0200

    Delete scr/routes/endDayRoutes.js

--- scr/routes/endDayRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { endDay, getAllDailyRevenue } = require('../controllers/dailyRevenueController');
-const router = express.Router();
-
-router.get('/', getAllDailyRevenue);
-router.post('/end-day', endDay);
-
-module.exports = router;
